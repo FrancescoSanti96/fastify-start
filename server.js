008e72bd47d017e74823534e22e7be3e44726ab7
Francesco Santi <francescosanti@MacBook-Francesco.local>
2023-11-05 11:23:09 +0100

    routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // ESM
 import Fastify from 'fastify'
+import firstRoute from './our-first-route.js'
 const fastify = Fastify({
   logger: true
 })
@@ -8,12 +9,14 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
+fastify.register(firstRoute)
+
 /**
  * Run the server!
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0'  })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
